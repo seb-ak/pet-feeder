1b19a1c95db1fc911a0e061cef8cf116caf59d09
SEB03penguin <SEB03penguin@gmail.com>
2026-01-28 17:16:03 +0000

    fixed fullscreen and can change title from python code

--- code/start.py
+++ code/start.py
@@ -3,16 +3,21 @@ from server import PetFeederServer
 import subprocess
 import os
 
-CONFIG = {
-    "CAPTURE_DEVICE": 0,
-    "FRAME_WIDTH": 1280 * 0.5,
-    "FRAME_HEIGHT": 720 * 0.5,
-    "FRAME_DELAY": 1/30,
-    "EXPECTED_PASSWORD": os.getenv("feeder_password", "password"),
+title = "Rabbit Feeder"
+videoWidth = 1280 * 0.5
+videoHeight = 720 * 0.5
+frameRate = 20
 
+CONFIG = {
+    "TITLE": title,
+    "EXPECTED_PASSWORD": os.getenv("feeder_password", "password"),
+    "FRAME_WIDTH": videoWidth,
+    "FRAME_HEIGHT": videoHeight,
+    "FRAME_DELAY": 1/frameRate,
+    "CAPTURE_DEVICE": 0,
     "SECRET_KEY": secrets.token_urlsafe(32),
-    "LOGIN_HTML": open("code/html/login.html").read(),
-    "DASHBOARD_HTML": open("code/html/dashboard.html").read(),
+    "LOGIN_HTML": open("code/html/login.html").read().replace("*title*", title),
+    "DASHBOARD_HTML": open("code/html/dashboard.html").read().replace("*title*", title),
 }
 
 subprocess.Popen(["cloudflared", "tunnel", "run", "rabbits"])
